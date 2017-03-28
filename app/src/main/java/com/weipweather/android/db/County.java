ef5f36870ca515afb7d32a3fa251d3f7541fcf64
zhangtaopp <857127401@qq.com>
2017-03-28 21:36:34 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/weipweather/android/db/County.java
@@ -0,0 +1,47 @@
+package com.weipweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2017/3/28 0028.
+ * 记录全国县城
+ */
+
+public class County extends DataSupport {
+    private int id;
+    private String countyName;
+    private String weatherId;
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
